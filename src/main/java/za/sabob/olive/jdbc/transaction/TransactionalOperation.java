c1cbda71dbea6a2ae83e035accad573e8642df91
Bob Schellink <sabob1@gmail.com>
2018-07-28 22:42:30 +0200

    added functionalinterface

--- src/main/java/za/sabob/olive/jdbc/transaction/TransactionalOperation.java
+++ src/main/java/za/sabob/olive/jdbc/transaction/TransactionalOperation.java
@@ -2,6 +2,7 @@ package za.sabob.olive.jdbc.transaction;
 
 import za.sabob.olive.jdbc.context.*;
 
+@FunctionalInterface
 public interface TransactionalOperation<X extends Exception> {
 
     public void run( JDBCContext ctx ) throws X;
--- src/main/java/za/sabob/olive/jdbc/transaction/TransactionalQuery.java
+++ src/main/java/za/sabob/olive/jdbc/transaction/TransactionalQuery.java
@@ -2,6 +2,7 @@ package za.sabob.olive.jdbc.transaction;
 
 import za.sabob.olive.jdbc.context.*;
 
+@FunctionalInterface
 public interface TransactionalQuery<T, X extends Exception> {
 
     public T get( JDBCContext ctx ) throws X;
